60f71fca25e2c8524145a0cd95a374dbb6f3233b
liqinshen9 <lshe727@aucklanduni.ac.nz>
2025-03-16 14:09:45 +1300

    set up timer

--- view.cpp
+++ view.cpp
@@ -3,5 +3,15 @@
 View::View(QQuickItem * parent):
     QQuickItem(parent)
 {
+    timer.setInterval(1000);
+    connect(&timer,SIGNAL(timeout()),this,SLOT(iterate()));
 
+    timer.start();
+
+}
+
+void View::iterate()
+{
+    counter += 1;
+    qDebug()<<counter;
 }
--- view.h
+++ view.h
@@ -1,6 +1,7 @@
 #ifndef VIEW_H
 #define VIEW_H
 
+#include <QTimer>
 #include <QQuickItem>
 
 class View: public QQuickItem
@@ -8,6 +9,13 @@ class View: public QQuickItem
     Q_OBJECT
 public:
     View(QQuickItem * parent = nullptr);
+
+private:
+    int counter  = 0;
+    QTimer timer;
+
+public slots:
+    void iterate();
 };
 
 #endif // VIEW_H
